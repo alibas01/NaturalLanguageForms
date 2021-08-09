e33dfdc3ece8b2fa4d10e443550dd849d4089a01
Ali Bas <alibas01@gmail.com>
2021-08-09 13:28:23 -0400

    static html set good to go

---
+++ server.js
@@ -0,0 +1,27 @@
+//exported functions
+
+// declared variables
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const path = require('path')
+const app = express();
+// app.use executions
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+const PORT = 2329;
+
+
+
+
+// GET /
+app.get("/", (req, res) => {
+  res.redirect("/static");
+});
+
+
+// server listen on port 8080
+app.listen(PORT, () => {
+  console.log(`NaturalLanguageForms listening on port ${PORT}!`);
+});
